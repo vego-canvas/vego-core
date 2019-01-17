533eb49286c5ef50829ca82e5d890c4d75738fee
汪韬 <452663934@qq.com>
2019-01-17 11:36:48 +0800

    :bug: 增加tweenmixin

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import VegoCanvas from './src/canvas';
 import DisplayObject from './src/render/DisplayObject';
 import { MouseEvent } from './src/event/Event';
+import TweenMixin from './src/tween/Tween';
 export {
     VegoCanvas,
     DisplayObject,
     MouseEvent,
+    TweenMixin,
 };
